56be756e4df050d7e7ac13ee1a788a3bd295548d
daniel <djl90037@gmail.com>
2022-06-18 11:08:39 -0500

    moved react code into src

--- template.js
+++ template.js
@@ -1,52 +1,80 @@
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand navbar-light bg-light">
-      <a className="navbar-brand" href="index.html">Navbar</a>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="press.html">Press</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+var Navbar = function Navbar() {
+  return React.createElement(
+    "nav",
+    { className: "navbar navbar-expand navbar-light bg-light" },
+    React.createElement(
+      "a",
+      { className: "navbar-brand", href: "index.html" },
+      "Navbar"
+    ),
+    React.createElement(
+      "div",
+      { className: "collapse navbar-collapse", id: "navbarNav" },
+      React.createElement(
+        "ul",
+        { className: "navbar-nav" },
+        React.createElement(
+          "li",
+          { className: "nav-item" },
+          React.createElement(
+            "a",
+            { className: "nav-link", href: "index.html" },
+            "Home"
+          )
+        ),
+        React.createElement(
+          "li",
+          { className: "nav-item" },
+          React.createElement(
+            "a",
+            { className: "nav-link", href: "press.html" },
+            "Press"
+          )
+        )
+      )
+    )
   );
-}
+};
 
-const Sidebar = () => {
-  return (
-    <div className="d-none d-md-block col-md-3">
-      <div className="border border-primary py-4 px-3">
-        Sidebar
-      </div>
-    </div>
-  )
-}
+var Sidebar = function Sidebar() {
+  return React.createElement(
+    "div",
+    { className: "d-none d-md-block col-md-3" },
+    React.createElement(
+      "div",
+      { className: "border border-primary py-4 px-3" },
+      "Sidebar"
+    )
+  );
+};
 
-const Footer = () => {
-  return (
-    <div className="border-top p-2">
-      Template Demo © 2019
-    </div>
-  )
-}
+var Footer = function Footer() {
+  return React.createElement(
+    "div",
+    { className: "border-top p-2" },
+    "Template Demo \xA9 2019"
+  );
+};
 
-const Template = (props) => {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <div className="container py-4">
-        <div className="row">
-          <div className="col-12 col-md-9">
-            {props.children}
-          </div>
-          <Sidebar />
-        </div>
-      </div>
-      <Footer />
-    </React.Fragment>
-  )
-}
+var Template = function Template(props) {
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(Navbar, null),
+    React.createElement(
+      "div",
+      { className: "container py-4" },
+      React.createElement(
+        "div",
+        { className: "row" },
+        React.createElement(
+          "div",
+          { className: "col-12 col-md-9" },
+          props.children
+        ),
+        React.createElement(Sidebar, null)
+      )
+    ),
+    React.createElement(Footer, null)
+  );
+};
